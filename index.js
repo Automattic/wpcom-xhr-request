d068d5da6fd983fdbcb9fd7597be1cc24f8514b1
retrofox <rdsuarez@gmail.com>
2016-07-18 19:57:08 -0300

    core: ES6ify

--- index.js
+++ index.js
@@ -2,28 +2,24 @@
 /**
  * Module dependencies.
  */
-
-var WPError = require('wp-error');
-var superagent = require('superagent');
-var debug = require('debug')('wpcom-xhr-request');
+import WPError from 'wp-error';
+import superagent from 'superagent';
+import debugFactory from 'debug';
 
 /**
- * Export a single `request` function.
+ * Module variables
  */
-
-module.exports = request;
+const debug = debugFactory( 'wpcom-xhr-request' );
 
 /**
  * WordPress.com REST API base endpoint.
  */
-
-var proxyOrigin = 'https://public-api.wordpress.com';
+let proxyOrigin = 'https://public-api.wordpress.com';
 
 /**
  * Default WordPress.com REST API Version.
  */
-
-var defaultApiVersion = '1';
+const defaultApiVersion = '1';
 
 /**
  * Performs an XMLHttpRequest against the WordPress.com REST API.
@@ -32,112 +28,107 @@ var defaultApiVersion = '1';
  * @param {Function} fn
  * @api public
  */
+export default function request( params, fn ) {
+	if ( 'string' === typeof params ) {
+		params = { path: params };
+	}
 
-function request (params, fn) {
+	const requestMethod = ( params.method || 'GET' ).toLowerCase();
+	debug( 'API HTTP Method: %o', requestMethod );
+	delete params.method;
 
-  if ('string' == typeof params) {
-    params = { path: params };
-  }
+	const apiVersion = params.apiVersion || defaultApiVersion;
+	delete params.apiVersion;
 
-  var method = (params.method || 'GET').toLowerCase();
-  debug('API HTTP Method: %o', method);
-  delete params.method;
+	const { apiNamespace } = params;
+	delete params.apiNamespace;
 
-  var apiVersion = params.apiVersion || defaultApiVersion;
-  delete params.apiVersion;
+	proxyOrigin = params.proxyOrigin || proxyOrigin;
+	delete params.proxyOrigin;
 
-  var apiNamespace = params.apiNamespace;
-  delete params.apiNamespace;
+	let basePath = '/rest/v' + apiVersion;
 
-  proxyOrigin = params.proxyOrigin || proxyOrigin;
-  delete params.proxyOrigin;
+	// If this is a WP-API request, adjust basePath
+	if ( apiNamespace && /\//.test( apiNamespace ) ) {
+		// New-style WP-API URL: /wpcom/v2/sites/%s/post-counts
+		basePath = '/' + apiNamespace;
+	} else if ( apiNamespace ) {
+		// Old-style WP-API URL (deprecated): /wp-json/sites/%s/wpcom/v2/post-counts
+		basePath = '/wp-json';
+	}
 
-  var basePath = '/rest/v' + apiVersion;
+	const url = proxyOrigin + basePath + params.path;
+	debug( 'API URL: %o', url );
+	delete params.path;
 
-  // If this is a WP-API request, adjust basePath
-  if ( apiNamespace && /\//.test( apiNamespace ) ) {
-    // New-style WP-API URL: /wpcom/v2/sites/%s/post-counts
-    basePath = '/' + apiNamespace;
-  } else if ( apiNamespace ) {
-    // Old-style WP-API URL (deprecated): /wp-json/sites/%s/wpcom/v2/post-counts
-    basePath = '/wp-json';
-  }
+	// create HTTP Request object
+	const req = superagent[ requestMethod ]( url );
 
-  var url = proxyOrigin + basePath + params.path;
-  debug('API URL: %o', url);
-  delete params.path;
+	// Token authentication
+	if ( params.authToken ) {
+		req.set( 'Authorization', 'Bearer ' + params.authToken );
+		delete params.authToken;
+	}
 
-  // create HTTP Request object
-  var req = superagent[method](url);
+	// URL querystring values
+	if ( params.query ) {
+		req.query( params.query );
+		debug( 'API send URL querystring: %o', params.query );
+		delete params.query;
+	}
 
-  // Token authentication
-  if (params.authToken) {
-    req.set('Authorization', 'Bearer ' + params.authToken);
-    delete params.authToken;
-  }
+	// POST API request body
+	if ( params.body ) {
+		req.send( params.body );
+		debug( 'API send POST body: %o', params.body );
+		delete params.body;
+	}
 
-  // URL querystring values
-  if (params.query) {
-    req.query(params.query);
-    debug('API send URL querystring: %o', params.query);
-    delete params.query;
-  }
+	// POST FormData (for `multipart/form-data`, usually a file upload)
+	if ( params.formData ) {
+		for ( let i = 0; i < params.formData.length; i++ ) {
+			const data = params.formData[ i ];
+			const key = data[ 0 ];
+			const value = data[ 1 ];
+			debug( 'adding FormData field %o', key );
+			req.field( key, value );
+		}
+	}
 
-  // POST API request body
-  if (params.body) {
-    req.send(params.body);
-    debug('API send POST body: %o', params.body);
-    delete params.body;
-  }
+	const requestHeaders = params.headers || {};
+	for ( const key in requestHeaders ) {
+		const value = requestHeaders[ key ];
+		debug( 'adding HTTP header %o: %o', key, value );
+		req.set( key, value );
+	}
 
-  // POST FormData (for `multipart/form-data`, usually a file upload)
-  if (params.formData) {
-    for (var i = 0; i < params.formData.length; i++) {
-      var data = params.formData[i];
-      var key = data[0];
-      var value = data[1];
-      debug('adding FormData field %o', key);
-      req.field(key, value);
-    }
-  }
+	if ( ! req.get( 'Accept' ) ) {
+		// set a default "Accept" header preferring a JSON response
+		req.set( 'Accept', '*/json,*/*' );
+	}
 
-  var headers = params.headers || {};
-  for (var key in headers) {
-    var value = headers[key];
-    debug('adding HTTP header %o: %o', key, value);
-    req.set(key, value);
-  }
+	// start the request
+	req.end( ( error, response ) => {
+		if ( error && ! response ) {
+			return fn( error );
+		}
 
-  if (!req.get('Accept')) {
-    // set a default "Accept" header preferring a JSON response
-    req.set('Accept', '*/json,*/*');
-  }
+		const { body, headers, statusCode } = response;
 
-  // start the request
-  req.end(function (err, res){
-    if (err && !res) {
-      return fn(err);
-    }
+		debug( '%o -> %o headers', url, headers );
+		debug( '%o -> %o status code', url, statusCode );
 
-    var body = res.body;
-    var headers = res.headers;
-    var statusCode = res.status;
-    debug('%o -> %o status code', url, statusCode);
+		if ( response.ok ) {
+			fn( null, body, headers );
+		} else {
+			headers.status = statusCode;
 
-    if (body && headers) {
-      body._headers = headers;
-    }
+			const { path, method } = response.req;
+			const wpe = WPError( { path, method }, statusCode, body );
 
-    if (res.ok) {
-      fn(null, body);
-    } else {
-      var wpe = WPError({
-        path: res.req.path, method: res.req.method
-      }, statusCode, body);
+			fn( wpe, null, headers );
+		}
+	} );
 
-      fn(wpe);
-    }
-  });
-
-  return req.xhr;
+	return req.xhr;
 }

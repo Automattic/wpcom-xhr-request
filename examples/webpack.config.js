c7420bf16037b4fe2e2e8da4f14764f4d72f87b0
Jarda Snajdr <jsnajdr@gmail.com>
2018-07-20 11:38:45 +0200

    Upgrade superagent, debug, webpack and serve packages
    Upgrades superagent and debug (production dependencies) and webpack and serve
    (dev dependencies) to their latest versions.
    
    Also removes some redundant code from the Webpack config file.
    
    The superagent and debug upgrades will be very useful in Calypso.

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,20 +1,10 @@
 module.exports = {
 	entry: __dirname + '/../build/index.js',
-
-	node: {
-		fs: 'empty'
-	},
-
 	output: {
 		path: __dirname + '/browser/dist/',
 		filename: 'wpcom-xhr-request.js',
 		libraryTarget: 'var',
 		library: 'WPCOMXhrHandler'
 	},
-
-	resolve: {
-		extensions: [ '', '.js' ]
-	},
-
 	devtool: 'sourcemap'
 };
